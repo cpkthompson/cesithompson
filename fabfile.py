eced000ed8d4f3aed673a427477d57b6c010a599
Charles Thompson <thompsoncharles@live.com>
2018-09-30 23:18:04 +0000

    Setup changes

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ def all():
 
 def prod():
   local('ng build --prod')
-  local('gcloud app deploy --project=sabya-co-za -q --account=choeysnax@gmail.com --verbosity=info')
+  local('gcloud app deploy --project=cesi-thompson -q --account=choeysnax@gmail.com --verbosity=info')
 
 
 def merge():
